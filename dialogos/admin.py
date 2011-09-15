a7c5c9d27d29e14ebb2d314890b40eab318501ac
Patrick Altman <paltman@gmail.com>
2011-09-15 06:58:30 -0500

    Update style to match Pinax conventions

--- dialogos/admin.py
+++ dialogos/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
+
 from dialogos.models import Comment
 
+
 class CommentAdmin(admin.ModelAdmin):  
-    list_display = ['author', 'content_type', 'public']
-    list_filter = ['public', 'content_type']
+    list_display = ["author", "content_type", "public"]
+    list_filter = ["public", "content_type"]
+
 
 admin.site.register(Comment, CommentAdmin)
