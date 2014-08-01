b0b580966d3cc46c213d6bd928ea69e7029f1496
Brian Rosner <brosner@gmail.com>
2014-07-31 21:30:44 -0600

    hooked up Travis

--- runtests.py
+++ runtests.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-import os
 import sys
 
 from django.conf import settings
@@ -7,29 +6,25 @@ from django.conf import settings
 
 if not settings.configured:
     settings.configure(
-        DATABASES={"default": {"ENGINE": "django.db.backends.sqlite3"}},
+        DATABASES={
+            "default": {
+                "ENGINE": "django.db.backends.sqlite3",
+            }
+        },
         ROOT_URLCONF="dialogos.urls",
         INSTALLED_APPS=[
             "django.contrib.auth",
             "django.contrib.contenttypes",
             "django.contrib.sessions",
             "dialogos",
-        ]
+        ],
     )
 
 
-from django.test.simple import DjangoTestSuiteRunner
+from django_nose import NoseTestSuiteRunner
 
+test_runner = NoseTestSuiteRunner(verbosity=1)
+failures = test_runner.run_tests(["dialogos"])
 
-def runtests(*test_args):
-    if not test_args:
-        test_args = ["dialogos"]
-    parent = os.path.dirname(os.path.abspath(__file__))
-    sys.path.insert(0, parent)
-    suite = DjangoTestSuiteRunner(verbosity=1, interactive=True)
-    failures = suite.run_tests(test_args)
+if failures:
     sys.exit(failures)
-
-
-if __name__ == "__main__":
-    runtests(*sys.argv[1:])
