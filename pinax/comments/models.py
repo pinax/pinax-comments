00f5977ea165becab23b5d25e95bf87cb9eba2c2
Thomas Schreiber <rizumu@users.noreply.github.com>
2015-10-01 07:45:15 +0200

    Support custom user models

--- pinax/comments/models.py
+++ pinax/comments/models.py
@@ -1,9 +1,9 @@
 from datetime import datetime
 
+from django.conf import settings
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 
-from django.contrib.auth.models import User
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 
@@ -11,7 +11,7 @@ from django.contrib.contenttypes.models import ContentType
 @python_2_unicode_compatible
 class Comment(models.Model):
 
-    author = models.ForeignKey(User, null=True, related_name="comments")
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, related_name="comments")
 
     name = models.CharField(max_length=100)
     email = models.CharField(max_length=255, blank=True)
