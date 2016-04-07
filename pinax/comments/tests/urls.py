b8f452474d94dcf13323220b16fed804d62872d2
khchine5 <hamzakhchine@gmail.com>
2016-04-07 16:23:35 +0100

    add namespace to avoid url include error

--- pinax/comments/tests/urls.py
+++ pinax/comments/tests/urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls import include, url
 from django.views.generic import TemplateView
 
-
 urlpatterns = [
-    url(r"^comment/", include("pinax.comments.urls")),
+    url(r"^comment/", include("pinax.comments.urls", namespace="pinax_comments")),
     url(r"^demo/$", TemplateView.as_view(), name="demo_detail"),
 ]
