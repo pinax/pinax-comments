4324287b9b594c0180d96d556fa1446986428a73
Ross Crawford-d'Heureuse <sendrossemail@gmail.com>
2020-06-28 23:03:24 +0200

    create ep serializer

--- pinax/comments/api/views.py
+++ pinax/comments/api/views.py
@@ -6,3 +6,8 @@ from pinax.comments.api import serializers
 class CommentViewSet(viewsets.ModelViewSet):
     model = Comment
     serializer_class = serializers.CommentSerializer
+
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return serializers.CommentInternalPrivateSerializer
+        return self.serializer_class
