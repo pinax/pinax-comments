b438e8c475f5a1fc62d1f649255f205789690d11
Petr Dlouhý <petr.dlouhy@email.cz>
2021-09-22 11:39:35 +0200

    fix slow admin if there are many users in DB

--- pinax/comments/admin.py
+++ pinax/comments/admin.py
@@ -6,6 +6,7 @@ from .models import Comment
 class CommentAdmin(admin.ModelAdmin):
     list_display = ["author", "content_type", "public"]
     list_filter = ["public", "content_type"]
+    autocomplete_fields = ['author']
 
 
 admin.site.register(Comment, CommentAdmin)
